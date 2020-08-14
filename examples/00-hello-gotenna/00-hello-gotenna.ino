c4620170ec03a955468873f9f179dff7b74832c9
Alec Murphy <almurphy@mailchuck.com>
2020-08-14 15:25:45 +0000

    Add file header and explanations

--- examples/00-hello-gotenna/00-hello-gotenna.ino
+++ examples/00-hello-gotenna/00-hello-gotenna.ino
@@ -1,3 +1,22 @@
+/*
+ * ESP32GTM library example 01 - Hello goTenna!
+ * 
+ * Demonstrates the default operation of the ESP32GTM library
+ * 
+ * On startup:
+ * - scan the BLE airspace for devices advertising goTenna services
+ * - connect to first GTM device found
+ * - blink lights on connected GTM
+ * In the main loop:
+ * - read and delete incoming messages
+ * - query device info (battery, temperature etc) periodically
+ * - if the GTM device disconnects, wait for it and reconnect
+ * 
+ * Informational messages are printed in the serial console.
+ * 
+ * https://gitlab.com/almurphy/ESP32GTM
+ */
+
 #include "ESP32GTM.h"
 
 void setup() {
